f1ecee451efc47cbda8c727c6a52f9bdeaf58f88
TIAGO\Tiago <tiagokost@gmail.com>
2018-10-04 12:54:38 -0300

    Parametrizando Header do request.

--- access-control-ui/src/Api.js
+++ access-control-ui/src/Api.js
@@ -1,3 +1,9 @@
 
 // Configurations API URLBASE
 export var ApiHostBase = 'http://18.224.123.42:8080/';
+
+export var HeaderRequest =  {
+    'Origin': 'http://127.0.0.1:3000',
+    'Authorization': 'Basic ZGV2c2NhcGU6MTIzNDU2',
+    'Content-Type': 'application/json'
+};
