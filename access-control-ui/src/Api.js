0847efe72953a3ea34f5f55f95eaa4828bbde162
TIAGO\Tiago <tiagokost@gmail.com>
2018-10-04 17:17:25 -0300

    Adicionando regra de autenticação do usuário.

--- access-control-ui/src/Api.js
+++ access-control-ui/src/Api.js
@@ -1,9 +1,27 @@
 
+import axios from "axios";
+import { getToken } from "./Auth";
+
 // Configurations API URLBASE
+//
+//
 export var ApiHostBase = 'http://18.224.123.42:8080/';
 
 export var HeaderRequest =  {
-    'Origin': 'http://127.0.0.1:3000',
     'Authorization': 'Basic ZGV2c2NhcGU6MTIzNDU2',
     'Content-Type': 'application/json'
 };
+
+const api = axios.create({
+  baseURL: ApiHostBase
+});
+
+api.interceptors.request.use(async config => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
